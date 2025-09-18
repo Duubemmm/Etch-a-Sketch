fa452596c158c92285a05344414db5ba235da675
Dubem <dubykol@gmail.com>
2025-09-18 23:48:20 +0100

    feat: add the 16x16 grid of square divs

---
+++ main.js
@@ -0,0 +1,19 @@
+// create the header
+const headingDiv = document.querySelector(".h1-div")
+const h1 = document.createElement("h1")
+h1.innerHTML = "Etch-a-Sketch"
+h1.classList.add("h1")
+headingDiv.appendChild(h1)
+
+// create the 16 by 16 grid
+const containerDiv = document.getElementById("container-div")
+
+function gridSquares(numberOfGrid){
+for(let i = 0; i < numberOfGrid; i++){
+    const div = document.createElement("div")
+    div.classList.add("grid-square")
+
+    containerDiv.appendChild(div)
+}
+}
+gridSquares(16);
